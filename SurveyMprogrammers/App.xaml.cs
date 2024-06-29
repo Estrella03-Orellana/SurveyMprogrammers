bd2a1ab700c1d1c94df777ae46d25454c1377b48
Estrella <orellanaestrella66@gmail.com>
2024-06-29 15:57:38 -0600

    Agregar archivos de proyecto.

---
+++ SurveyMprogrammers/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyMprogrammers
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
