f413b28170f88832d18cd87b3015a9a613d830e6
Shele - MINEDUCYT <1925597@clases.edu.sv>
2024-06-29 17:53:07 -0600

    Se añadió suscripción al mensaje NewSurveyComplete desde SurveysView

--- SurveyMprogrammers/SurveysView.xaml.cs
+++ SurveyMprogrammers/SurveysView.xaml.cs
@@ -5,6 +5,10 @@ public partial class SurveysView : ContentPage
 	public SurveysView()
 	{
 		InitializeComponent();
+		MessagingCenter.Subscribe<ContentPage, Surveys>(this, Messages.NewSurveyComplete, (sender, args) =>
+		{
+			SurveysPanel.Children.Add(new Label() { Text = args.ToString() });
+		});
 	}
 
     private async void AddSurveyButton_Clicked(object sender, EventArgs e)
