628223038ecc290e8bff8091f02ab583f0fdb819
EmelyNj <23emelynajera@gmail.com>
2024-06-29 17:19:26 -0600

    Se creo el arreglo teams en el .cs de SurveyDetailsView y se modifico el boton de FavoriteTeam

--- SurveyMprogrammers/SurveyDetailsView.xaml.cs
+++ SurveyMprogrammers/SurveyDetailsView.xaml.cs
@@ -2,6 +2,18 @@ namespace SurveyMprogrammers;
 
 public partial class SurveyDetailsView : ContentPage
 {
+    private readonly string[] teams =
+    {
+        "Alianza Lima",
+        "América",
+        "Boca Juniors",
+        "Caracas FC",
+        "Colo-Colo",
+        "Peñarol",
+        "Real Madrid",
+        "Saprissa"
+    };
+
 	public SurveyDetailsView()
 	{
 		InitializeComponent();
@@ -9,8 +21,13 @@ public partial class SurveyDetailsView : ContentPage
 
    
 
-    private void FavoriteTeam_Clicked(object sender, EventArgs e)
+    private async void FavoriteTeam_Clicked(object sender, EventArgs e)
     {
+        var favoriteTeam = await DisplayActionSheet(Literals.FavoriteTEamTitle, null, null, teams);
+        if (!string.IsNullOrWhiteSpace(favoriteTeam))
+        {
+            FavoriteTeamLabel.Text = favoriteTeam;
+        }
 
     }
 
